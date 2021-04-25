81fb77dc47de88d26e43170bc00c00bef0fb325f
alt-ROCK <56280184+aaltrock@users.noreply.github.com>
2021-04-25 06:36:31 +0100

    Task 2.5: WIP

--- Task 2.3 onwards/task_2_5_main.py
+++ Task 2.3 onwards/task_2_5_main.py
@@ -14,21 +14,21 @@ import pprint
 # Function adapted based on
 # Source: E. Jiménez-Ruiz, “INM713 Semantic Web Technologies and Knowledge Graphs
 # Laboratory 8: Ontology Alignment,” 2021.
-def calc_scores(ls_1, ls_2):
+def calc_scores(ref_ls, local_ls):
     # Set True and False +ve and -ve to zero
     tp, tn, fp, fn = 0, 0, 0, 0
     # Note: for completeness added true -ve but will always be zero
 
-    # Check for value in list 1 that it is in list 2
-    for v1 in ls_1:
-        if v1 in ls_2:
+    # Check for value in ref and local lists
+    for v1 in local_ls:
+        if v1 in ref_ls:
             tp += 1     # Both have in their lists
         else:
-            fp += 1     # In list 1 but not in 2
+            fp += 1     # In local list but not in ref list
 
-    for v2 in ls_2:
-        if v2 in ls_1:
-            fn += 1     # In list 2 but not in 1
+    for v2 in ref_ls:
+        if v2 not in local_ls:
+            fn += 1     # In ref list but not in local list
 
     # Calculate precision, recall and f1 score while handing div by 0 error
     precision = tp/(tp + fn) if (tp + fn) != 0 else .0
@@ -43,11 +43,16 @@ def calc_scores(ls_1, ls_2):
 
 
 # Extract classes (excluding blank nodes)
-def extract_class(g):
+def extract_class(g, delim='#'):
     cls_ls = []
+    # Extract classes by finding the RDF.type definition triples with OWL.class
     for s, p, o in g:
         if p == RDF.type and o == OWL.Class and type(s) != BNode:
             cls_ls += [s]
+
+    # Get the entity from URI based on delimiter
+    cls_ls = [(uri.split(delim)[-1], uri, 'class') for uri in cls_ls]
+
     return cls_ls
 
 
@@ -55,26 +60,41 @@ def main():
     """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
             TASK 2.5 SUBTASK SPARQL.1
     """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
-    # Load in the two ontologies - own and external OWLs
-    ext_g = rdflib.Graph()
-    own_g = rdflib.Graph()
+    # Load in the two ontologies - own and external reference OWLs
+    ref_g = rdflib.Graph()
+    local_g = rdflib.Graph()
 
-    ext_g.parse('pizza.owl.xml', format='xml')
-    own_g.parse('2.4_sparql1_g.ttl', format='ttl')
+    ref_g.parse('pizza.owl.xml', format='xml')
+    local_g.parse('2.4_sparql1_g.ttl', format='ttl')
 
     # Check similarity by class
     # Extract classes from both graphs
-    ext_cls_ls = extract_class(ext_g)
-    own_cls_ls = extract_class(own_g)
+    ext_cls_ls = extract_class(ref_g)
+    own_cls_ls = extract_class(local_g)
 
-    cls_f1 = calc_scores(ext_cls_ls, own_cls_ls)
+    # Extract objects from both graphs
 
-    print('Lexical matching scores between classes:')
+    # Extract properties from both graphs
+
+    # Extract individuals from both graphs
+
+    # Match by every permutation among class, obj, properties, individuals
+
+
+    # Calculate based on if own defined ontology exists in the external one (the standard)
+    cls_f1 = calc_scores(own_cls_ls, ext_cls_ls)
+
+    print('Lexical based matching scores between classes:')
     pprint.pprint(cls_f1)
 
     # Check similarity by object
 
+
     # Check similarity by properties
+
+
+    # Check similarity by individuals
+
 
 
     print('END')
